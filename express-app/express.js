f897423731d6a6892f02e8f4e61c56a6c9b99e13
bngams <n.boris@devidia.net>
2022-05-13 16:59:18 +0200

    add cors

--- express-app/express.js
+++ express-app/express.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var cors = require('cors');
 const todosCtrl = require('./Todos/todo-controller');
 const postsCtrl = require('./Posts/post.controller');
 const app = express();
@@ -7,6 +8,7 @@ const port = 8282;
 
 // use body-parser for express
 app.use(bodyParser.json()); // for parsing application/json
+app.use(cors());
 
 // use routes in todoCtrl
 app.use('/todos', todosCtrl);
